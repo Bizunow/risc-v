57ca93b8043699c2c3ca26a72da9217fe21bbaef
Ilya Bizunov <bizunow13@gmail.com>
2025-10-09 15:16:08 +0300

    Tests for LB & LBU

--- tests/cpu.test.ts
+++ tests/cpu.test.ts
@@ -17,7 +17,6 @@ describe('CPU', () => {
 
     test('Register x0 should be immutable', () => {
         // ADDI x0, x0, 42 (0x02a00013)
-        // This adds 42 to register x0 (which is always 0) and stores in x1
         memory.write(0, 0x13);
         memory.write(1, 0x00);
         memory.write(2, 0xa0);
@@ -31,7 +30,6 @@ describe('CPU', () => {
 
     test('Should execute ADDI instruction', () => {
         // ADDI x1, x0, 42 (0x02a00093)
-        // This adds 42 to register x0 (which is always 0) and stores in x1
         memory.write(0, 0x93);
         memory.write(1, 0x00);
         memory.write(2, 0xa0);
@@ -42,5 +40,37 @@ describe('CPU', () => {
         cpu.dump = jest.fn(); // Mock dump to avoid console output
         expect(cpu.getRegisterValue(0)).toBe(0);
         expect(cpu.getRegisterValue(1)).toBe(42);
+    });
+
+    test('Load byte signed', () => {
+        // 0x00500083
+        // lb x1, 5(x0)
+        memory.write(0, 0x83);
+        memory.write(1, 0x00);
+        memory.write(2, 0x50);
+        memory.write(3, 0x00);
+        
+        memory.write(5, 0xff);
+
+        cpu.step();
+
+        // cpu.dump = jest.fn(); // Mock dump to avoid console output
+        expect(cpu.getRegisterValue(1)).toBe(-1);
+    });
+
+    test('Load byte unsigned', () => {
+        // 0x00504083
+        // lbu x1, 5(x0)
+        memory.write(0, 0x83);
+        memory.write(1, 0x40);
+        memory.write(2, 0x50);
+        memory.write(3, 0x00);
+        
+        memory.write(5, 0xff);
+
+        cpu.step();
+
+        // cpu.dump = jest.fn(); // Mock dump to avoid console output
+        expect(cpu.getRegisterValue(1)).toBe(0xff);
     });
 });
