ad5bf088f517becce49cacc17d29a0f0fb53620a
Ilya Bizunov <bizunow13@gmail.com>
2025-10-11 12:16:44 +0300

    Jest coverage fix

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,20 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/src', '<rootDir>/tests'],
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      tsconfig: {
+        sourceMap: true,
+        inlineSourceMap: false
+      }
+    }]
+  },
   moduleNameMapper: {
     '^@src/(.*)$': '<rootDir>/src/$1'
   },
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/*.d.ts'
-  ]
+  ],
+  coverageProvider: 'v8'
 };
